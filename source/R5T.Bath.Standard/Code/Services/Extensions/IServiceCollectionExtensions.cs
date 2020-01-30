18ac678499170bcab6a9aca33e74b4787bd0313e
MinexAutomation <minex.automation@gmail.com>
2020-01-30 02:45:45 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Bath.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Bath.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Bath.File.Standard;
+using R5T.Dacia;
 
 
 namespace R5T.Bath.Standard
@@ -17,6 +18,15 @@ namespace R5T.Bath.Standard
             services.AddFileHumanOutput();
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds a file-based <see cref="IHumanOutput"/> service.
+        /// </summary>
+        public static ServiceAction<IHumanOutput> AddHumanOutputAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IHumanOutput>(() => services.AddHumanOutput());
+            return serviceAction;
         }
     }
 }
